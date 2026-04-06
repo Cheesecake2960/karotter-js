5bd6962cf8e25eaf727a55e93fe625acd15e448d
Cheesecake <155858568+Cheesecake2960@users.noreply.github.com>
2026-04-06 09:25:18 +0900

    コメントを追加

--- types/question.d.ts
+++ types/question.d.ts
@@ -2,11 +2,24 @@ import type { UserType } from "./user.js"
 import type { PostType } from "./post.js"
 
 export type QuestionType = {
+  /** 質問の回答日時 (ISO) */
   answeredAt: string | null
+
+  /** 質問に回答した投稿のIDなどを含む */
   answeredPost: { author: {username: UserType["username"]}, id: PostType["id"] } | null
+  
+  /** 質問の本文 */
   content: string
+
+  /** 質問の作成日時 (ISO) */
   createdAt: string
+
+  /** 質問のID */
   id: number
+
+  /** 質問が読まれたかどうか */
   isRead: boolean
+
+  /** 質問をしたユーザーの情報 */
   sender: Pick<UserType, "id" | "username" | "displayName" | "avatarFrameId" | "avatarUrl" | "officialMark">
 }
