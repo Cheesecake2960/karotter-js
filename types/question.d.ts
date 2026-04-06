f5165703a3ed42f78c01e27dd9b16a24448f5c03
Cheesecake <155858568+Cheesecake2960@users.noreply.github.com>
2026-04-06 09:23:55 +0900

    QuestionTypeのtypoを修正

--- types/question.d.ts
+++ types/question.d.ts
@@ -2,8 +2,8 @@ import type { UserType } from "./user.js"
 import type { PostType } from "./post.js"
 
 export type QuestionType = {
-  answerdAt: string | null
-  answerdPost: { author: {username: UserType["username"]}, id: PostType["id"] } | null
+  answeredAt: string | null
+  answeredPost: { author: {username: UserType["username"]}, id: PostType["id"] } | null
   content: string
   createdAt: string
   id: number
